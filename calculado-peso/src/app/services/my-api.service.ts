0926c278fb5f5179eb70ae2295d88e2343bde892
Ruan Lauro <ruanlauromonteiro@gmail.com>
2025-07-10 18:40:51 -0300

    update

--- calculado-peso/src/app/services/my-api.service.ts
+++ calculado-peso/src/app/services/my-api.service.ts
@@ -1,33 +1,65 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { environment } from '../../../environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MyApiService {
-
-  private apiUrl = 'http://localhost:5096/api/Co2';
-
-  constructor(private http: HttpClient) { }
+  
+  private apiUrl = environment.apiUrl;
+  
+  constructor(
+    private http: HttpClient,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
 
   getDados(): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/`);
+    if (isPlatformBrowser(this.platformId)) {
+      return this.http.get<any>(`${this.apiUrl}/`);
+    } else {
+      return of([]);
+    }
   }
 
   getItemPorId(id: number): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/dados/${id}`);
+    if (isPlatformBrowser(this.platformId)) {
+      return this.http.get<any>(`${this.apiUrl}/dados/${id}`);
+    } else {
+      return of(null);
+    }
   }
 
-  criarItem(dado: {origem: string, destino: string, formatoDeContainers: string, tipoContainers: string, carregamento: string, quantidadeTEUs:number}): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/`, dado);
+  criarItem(dado: {
+    origem: string,
+    destino: string,
+    formatoDeContainers: string,
+    tipoContainers: string,
+    carregamento: string,
+    quantidadeTEUs: number
+  }): Observable<any> {
+    if (isPlatformBrowser(this.platformId)) {
+      return this.http.post<any>(`${this.apiUrl}/`, dado);
+    } else {
+      return of(null);
+    }
   }
 
   atualizarItem(id: number, dado: any): Observable<any> {
-    return this.http.put<any>(`${this.apiUrl}/dados/${id}`, dado);
+    if (isPlatformBrowser(this.platformId)) {
+      return this.http.put<any>(`${this.apiUrl}/dados/${id}`, dado);
+    } else {
+      return of(null);
+    }
   }
 
   deletarItem(id: number): Observable<any> {
-    return this.http.delete<any>(`${this.apiUrl}/dados/${id}`);
+    if (isPlatformBrowser(this.platformId)) {
+      return this.http.delete<any>(`${this.apiUrl}/dados/${id}`);
+    } else {
+      return of(null);
+    }
   }
 }
