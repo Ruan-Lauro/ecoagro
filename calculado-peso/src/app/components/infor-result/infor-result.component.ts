b416a4dcbe6f862ffc6457b81aa2130030e69c78
Ruan Lauro <ruanlauromonteiro@gmail.com>
2025-07-10 18:39:34 -0300

    update

--- calculado-peso/src/app/components/infor-result/infor-result.component.ts
+++ calculado-peso/src/app/components/infor-result/infor-result.component.ts
@@ -12,6 +12,29 @@ import { LoadingComponent } from "../loading-component/loading-component.compone
 })
 export class InforResultComponent {
 
+    exemple = {
+    "quantidade": 5,
+    "formato": "dry",
+    "carregamento": "TEU",
+    "origem": "Picos - PI",
+    "destino": "Rio de Janeiro - RJ",
+    "porto_origem": "São Gonçalo do Amarante",
+    "porto_destino": "Itaguaí",
+    "transit_time": "Plata",
+    "rel_rodoviario": 8.922614,
+    "rel_porta_porto": 2.2025862,
+    "rel_ativ_porto": 0.26803184,
+    "rel_cabotagem": 1.637008,
+    "rel_porto_porta": 0.3139647,
+    "total_rodoviario": 8.922614,
+    "total_mercosul": 4.421591,
+    "economia": 4.5,
+    "arvores": 74,
+    "gelo": 13.5,
+    "caminhoes": 3,
+    "credito_carbono": 4
+  };
+
   @Input() data!: Transport;
 
   Caminhaoloading = false; 
@@ -19,53 +42,55 @@ export class InforResultComponent {
   constructor(private pdfService: PdfService) {}
 
   get formattedData(): Transport {
+    const source = this.data ?? this.exemple;
     return {
-      ...this.data,
-      rel_rodoviario: Number(this.data.rel_rodoviario.toFixed(2)),
-      rel_porta_porto: Number(this.data.rel_porta_porto.toFixed(2)),
-      rel_ativ_porto: Number(this.data.rel_ativ_porto.toFixed(2)),
-      rel_cabotagem: Number(this.data.rel_cabotagem.toFixed(2)),
-      rel_porto_porta: Number(this.data.rel_porto_porta.toFixed(2)),
-      total_rodoviario: Number(this.data.total_rodoviario.toFixed(2)),
-      total_mercosul: Number(this.data.total_mercosul.toFixed(2)),
-      economia: Number(this.data.economia.toFixed(2)),
-      arvores: Number(this.data.arvores.toFixed(2)),
-      gelo: Number(this.data.gelo.toFixed(2)),
-      caminhoes: Number(this.data.caminhoes.toFixed(2)),
-      credito_carbono: Number(this.data.credito_carbono.toFixed(2)),
-      quantidade:this.data.quantidade,
-      formato: this.data.formato,
-      carregamento: this.data.carregamento,
-      origem: this.data.origem,
-      destino: this.data.destino,
-      porto_origem: this.data.porto_origem,
-      porto_destino: this.data.porto_destino,
-      transit_time: this.data.transit_time,
+      ...source,
+      rel_rodoviario: Number(source.rel_rodoviario.toFixed(2)),
+      rel_porta_porto: Number(source.rel_porta_porto.toFixed(2)),
+      rel_ativ_porto: Number(source.rel_ativ_porto.toFixed(2)),
+      rel_cabotagem: Number(source.rel_cabotagem.toFixed(2)),
+      rel_porto_porta: Number(source.rel_porto_porta.toFixed(2)),
+      total_rodoviario: Number(source.total_rodoviario.toFixed(2)),
+      total_mercosul: Number(source.total_mercosul.toFixed(2)),
+      economia: Number(source.economia.toFixed(2)),
+      arvores: Number(source.arvores.toFixed(2)),
+      gelo: Number(source.gelo.toFixed(2)),
+      caminhoes: Number(source.caminhoes.toFixed(2)),
+      credito_carbono: Number(source.credito_carbono.toFixed(2)),
+      quantidade: source.quantidade,
+      formato: source.formato,
+      carregamento: source.carregamento,
+      origem: source.origem,
+      destino: source.destino,
+      porto_origem: source.porto_origem,
+      porto_destino: source.porto_destino,
+      transit_time: source.transit_time,
     };
   }
 
   async generatePdf() {
+    const source = this.data ?? this.exemple;
     const dataPdf = {
-      rel_rodoviario: Number(this.data.rel_rodoviario.toFixed(2)),
-      rel_porta_porto: Number(this.data.rel_porta_porto.toFixed(2)),
-      rel_ativ_porto: Number(this.data.rel_ativ_porto.toFixed(2)),
-      rel_cabotagem: Number(this.data.rel_cabotagem.toFixed(2)),
-      rel_porto_porta: Number(this.data.rel_porto_porta.toFixed(2)),
-      total_rodoviario: Number(this.data.total_rodoviario.toFixed(2)),
-      total_mercosul: Number(this.data.total_mercosul.toFixed(2)),
-      economia: Number(this.data.economia.toFixed(2)),
-      arvores: Number(this.data.arvores.toFixed(2)),
-      gelo: Number(this.data.gelo.toFixed(2)),
-      caminhoes: Number(this.data.caminhoes.toFixed(2)),
-      credito_carbono: Number(this.data.credito_carbono.toFixed(2)),
-      quantidade:this.data.quantidade,
-      formato: this.data.formato,
-      carregamento: this.data.carregamento,
-      origem: this.data.origem,
-      destino: this.data.destino,
-      porto_origem: this.data.porto_origem,
-      porto_destino: this.data.porto_destino,
-      transit_time: this.data.transit_time,
+        rel_rodoviario: Number(source.rel_rodoviario.toFixed(2)),
+        rel_porta_porto: Number(source.rel_porta_porto.toFixed(2)),
+        rel_ativ_porto: Number(source.rel_ativ_porto.toFixed(2)),
+        rel_cabotagem: Number(source.rel_cabotagem.toFixed(2)),
+        rel_porto_porta: Number(source.rel_porto_porta.toFixed(2)),
+        total_rodoviario: Number(source.total_rodoviario.toFixed(2)),
+        total_mercosul: Number(source.total_mercosul.toFixed(2)),
+        economia: Number(source.economia.toFixed(2)),
+        arvores: Number(source.arvores.toFixed(2)),
+        gelo: Number(source.gelo.toFixed(2)),
+        caminhoes: Number(source.caminhoes.toFixed(2)),
+        credito_carbono: Number(source.credito_carbono.toFixed(2)),
+        quantidade: source.quantidade,
+        formato: source.formato,
+        carregamento: source.carregamento,
+        origem: source.origem,
+        destino: source.destino,
+        porto_origem: source.porto_origem,
+        porto_destino: source.porto_destino,
+        transit_time: source.transit_time,
     };
     
     try {
